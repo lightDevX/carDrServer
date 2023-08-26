faaea0ea6f65b2f026f351bc009b395c3529b3d8
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2023-08-26 14:25:23 +0600

    database linking

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const serviceCollections = client.db("Server-CarDR").collection("services");
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
